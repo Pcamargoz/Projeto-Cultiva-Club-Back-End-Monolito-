cea8c2756a244a2c4e75deb0e2def7659de12834
Pedro Camargo <pedro01cesarsanto@gmail.com>
2026-04-22 19:27:28 -0300

    Arrumando erro oauth 2

--- src/main/java/com/cultivaclub/monolito/common/config/SecurityConfiguration.java
+++ src/main/java/com/cultivaclub/monolito/common/config/SecurityConfiguration.java
@@ -8,8 +8,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -25,11 +23,6 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
     private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(11);
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
